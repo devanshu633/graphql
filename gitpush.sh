a04e8c84f56ddf3ba6f0e73d6861e88d1336b28f
devanshu-pedalsup <devanshu.patel@pedalsup.com>
2023-01-13 10:46:17 +0530

    auth added

---
+++ gitpush.sh
@@ -0,0 +1,16 @@
+echo 'Enter the commit message:'
+read commitMessage
+
+echo 'Percentage Done:'
+read Done
+
+echo 'Working Hours:'
+read Hours
+
+git commit -m "$commitMessage; Done:$Done%; Hours:$Hours"
+
+echo 'Enter the name of the branch:'
+read branch
+
+git push origin $branch
+
